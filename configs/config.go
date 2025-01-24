241f8555aae56219619dc4f731999b35971284fc
AgamariFF <Laminanonono@mail.ru>
2025-01-24 11:39:56 +0300

    changed docker files

--- configs/config.go
+++ configs/config.go
@@ -9,11 +9,7 @@ import (
 
 type Config struct {
 	ServerPort string
-	DBHost     string
-	DBPort     string
-	DBUser     string
-	DBPassword string
-	DBName     string
+	DBDSN      string
 }
 
 func LoadConfig() Config {
@@ -25,11 +21,7 @@ func LoadConfig() Config {
 	// Чтение переменных из окружения
 	config := Config{
 		ServerPort: getEnv("SERVER_PORT", "8080"),
-		DBHost:     getEnv("DB_HOST", "localhost"),
-		DBPort:     getEnv("DB_PORT", "5432"),
-		DBUser:     getEnv("DB_USER", "postgres"),
-		DBPassword: getEnv("DB_PASSWORD", ""),
-		DBName:     getEnv("DB_NAME", "library"),
+		DBDSN:      getEnv("DB_DSN", "localhost"),
 	}
 
 	return config
